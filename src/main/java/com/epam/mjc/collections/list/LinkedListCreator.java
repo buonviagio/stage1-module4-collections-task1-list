3b2c894e5c407c812dd7f9992e69d166d86b4f95
Shapovalov Dmytro <buonviagio@gmail.com>
2023-03-23 12:58:10 +0200

    My first commit Stage1 Module#Collection Task#ListInterface

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -1,9 +1,19 @@
 package com.epam.mjc.collections.list;
 
+
 import java.util.LinkedList;
 import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (Integer i : sourceList) {
+            if (i % 2 != 0) {
+                list.add(0, i);
+            } else {
+                list.add(list.size(), i);
+            }
+        }
+        return list;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,23 @@
 package com.epam.mjc.collections.list;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class ListSorter {
+
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int first = Integer.parseInt(a), second = Integer.parseInt(b);
+        int resultA = (5 * (first * first) + 3);
+        int resultB = (5 * (second * second) + 3);
+        if (resultA == resultB) {
+            return -1;
+        }
+        return resultA - resultB;
     }
 }
